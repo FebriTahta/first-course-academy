b190094cdcc2c46d8d404c4328e0847b91542dc9
Febri Tahta <febrirtah@gmail.com>
2020-11-25 23:57:21 -0800

    tes meta og

--- resources/views/layouts/c_layouts/navbar.blade.php
+++ resources/views/layouts/c_layouts/navbar.blade.php
@@ -10,7 +10,7 @@
     <meta property="og:type"               content="article" />
     <meta property="og:title"              content="Course Academy" />
     <meta property="og:description"        content="Kini kami hadir dalam bentuk digital untuk memenuhi kebutuhan belajar anda" />
-    <meta property="og:image"              content="{{ asset('gawr/Free-Online-Course.jpg') }}" />
+    <meta property="og:image"              content="{{ asset('gawr/gawr.jpg') }}"/>
     <title>Course Academy</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet">
